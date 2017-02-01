010570d97fce190b2601018a806b70f4ce48a408
55heyerjt01 <55heyerjt01@MUP-R-EH207L23.PASSHE.LCL>
2017-02-01 09:08:25 -0500

    Update Guess.java to randomize the answer every build.

--- HW2017/src/Week1/Guess.java
+++ HW2017/src/Week1/Guess.java
@@ -1,5 +1,6 @@
 /**
  * Run a number Guessing Game.
+ *
  * @author Jason Heyer
  */
 package Week1;
@@ -7,18 +8,29 @@ package Week1;
 import java.util.Scanner;
 
 public class Guess {
-     public static void main(String[] args) {
-         System.out.print("Can you guess the correct number between 1 and 10? Guess: ");
-            Scanner input = new Scanner(System.in);
-            int guess = input.nextInt();
-            int answer = 9; 
-            
-            while(guess != answer){
-                System.out.print("Sorry, guess again: ");
+
+    public static void main(String[] args) {
+        System.out.print("Can you guess the correct number between 1 and 10? Guess: ");
+        Scanner input = new Scanner(System.in);
+        int guess = input.nextInt();
+        int answer = (int)(Math.random() * 10);
+
+        while (guess != answer) {
+//            if (guess != answer) {
+//                System.out.print("Sorry, guess again: ");
+//                guess = input.nextInt(); }
+            if (guess < answer) {
+                System.out.print("Too low, guess a bigger number: ");
                 guess = input.nextInt();
-            } 
-            if (guess == answer){
-                System.out.println("*~-._ Correct!!! _.-~* Great Guessing!");
+               // break;
+            }else {
+                System.out.print("Too big, guess a lower number: ");
+                guess = input.nextInt();
+               // break;
             }
+        }
+        System.out.println("*~-._ Correct!!! _.-~* Great Guessing!");
     }
+
 }
+
