1500b6964666930b5db89d6486166524036d9b46
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2017-02-01 19:29:39 -0500

    Update Guess.java

--- HW2017/src/Week1/Guess.java
+++ HW2017/src/Week1/Guess.java
@@ -10,23 +10,18 @@ import java.util.Scanner;
 public class Guess {
 
     public static void main(String[] args) {
-        System.out.print("Can you guess the correct number between 1 and 10? Guess: ");
+        System.out.print("What is the correct number between 1 and 10? Guess: ");
         Scanner input = new Scanner(System.in);
         int guess = input.nextInt();
         int answer = (int)(Math.random() * 10);
 
         while (guess != answer) {
-//            if (guess != answer) {
-//                System.out.print("Sorry, guess again: ");
-//                guess = input.nextInt(); }
             if (guess < answer) {
                 System.out.print("Too low, guess a bigger number: ");
                 guess = input.nextInt();
-               // break;
             }else {
                 System.out.print("Too big, guess a lower number: ");
                 guess = input.nextInt();
-               // break;
             }
         }
         System.out.println("*~-._ Correct!!! _.-~* Great Guessing!");
