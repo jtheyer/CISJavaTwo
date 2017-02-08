f9ce9c0f9ea8782f5a361185446175c12129474c
55heyerjt01 <55heyerjt01@MUP-R-EH207L23.PASSHE.LCL>
2017-02-08 09:16:49 -0500

    Add ChessUI.java

---
+++ HW2017/src/Week3/ChessUI.java
@@ -0,0 +1,12 @@
+/**
+ * Create a chess board (8x8)
+ *
+ * @author Jason Heyer
+ */
+package Week3;
+
+public class ChessUI {
+    public static void main(String[] args) {
+        ChessBoard chess = new ChessBoard(); 
+    }
+}
