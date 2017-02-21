e719816bc94a741a3263bb33c6fe227a146504c8
55heyerjt01 <55heyerjt01@MUP-R-EH207L23.PASSHE.LCL>
2017-02-21 08:45:49 -0500

    Update NumberFormatEx.java

--- HW2017/src/Week4Ch12/NumberFormatEx.java
+++ HW2017/src/Week4Ch12/NumberFormatEx.java
@@ -11,9 +11,41 @@ package Week4Ch12;
 import java.util.Scanner;
 
 public class NumberFormatEx {
+
+    public static int hexToDecimal(String hex) {
+        int decimalValue = 0;
+        for (int i = 0; i < hex.length(); i++) {
+            char hexChar = hex.charAt(i);
+            decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
+        }
+        if (hex.length() != 4) 
+            throw new IllegalArgumentException();
+        
+        return decimalValue;
+    }
+
+    public static int hexCharToDecimal(char ch) {
+        if (ch >= 'A' && ch <= 'F') {
+            return 10 + ch - 'A';
+        } else {
+            return ch - '0';
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Input hex string here: ");
-        int hex = input.nextInt();
+        boolean continueInput = true;
+        do {
+            try {
+                System.out.print("Input hex string here: ");
+                String hex = input.next();
+                System.out.println("Decimal value for hex number is: " + hexToDecimal(hex.toUpperCase()));
+                continueInput = false;
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Exception: Not a hex string!");
+                input.nextLine();
+            }
+        } while (continueInput);
     }
 }
