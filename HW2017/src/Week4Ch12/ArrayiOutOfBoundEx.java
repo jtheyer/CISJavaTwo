3b90e71d15adea2c2d1a5ee1a41b9b8f738c83df
jason <heyerjt01@mounties.mansfield.edu>
2017-02-21 00:15:45 -0500

    Add ArrayiOutOfBoundEx.java

---
+++ HW2017/src/Week4Ch12/ArrayiOutOfBoundEx.java
@@ -0,0 +1,42 @@
+/**
+ * Page 488 #12.4
+ * Array index out of bounds exception
+ * Create array w/ 100 random #'s, prompt for index and
+ *  return corresponding element.
+ *
+ * @author Jason Heyer
+ */
+package Week4Ch12;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayiOutOfBoundEx {
+
+    public static int returnIndex(ArrayList<Integer> list, int index) {
+        index = list.get(index);
+        return index;
+    }
+
+    public static void main(String[] args) {
+        boolean continueInput = true;
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int e = (int) (Math.random() * 100);
+            list.add(e);
+        }
+        System.out.println(list);
+        do{
+            try{
+                System.out.print("Choose an element index: ");
+            int index = input.nextInt();
+            System.out.println(returnIndex(list, index));
+            continueInput = false;
+            }catch(ArrayIndexOutOfBoundsException ex){
+                System.out.println("Out of Bounds");
+                input.nextLine(); //Discard input
+            }
+        }while(continueInput);
+    }
+}
