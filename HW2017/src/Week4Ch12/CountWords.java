4090fd925abef071baf7e3cd080615f258a514de
55heyerjt01 <55heyerjt01@MUP-R-EH207L23.PASSHE.LCL>
2017-02-27 07:54:28 -0500

    Finish CountWords.java

--- HW2017/src/Week4Ch12/CountWords.java
+++ HW2017/src/Week4Ch12/CountWords.java
@@ -13,23 +13,37 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class CountWords {
-//    private static int words;
-    public static int countWords(String urlName) throws MalformedURLException, IOException{
-          java.net.URL url = new java.net.URL(urlName);
-          Scanner urlInput = new Scanner(url.openStream());
-          int words = 0;
-          return words;
-    
+
+    private static int sumWords = 0;
+
+    public static int countWords(String urlName)
+            throws MalformedURLException, IOException {
+        try {
+            java.net.URL url = new java.net.URL(urlName);
+            try{
+            Scanner urlInput = new Scanner(url.openStream());
+            while(urlInput.hasNext()){
+                String words = urlInput.next();
+                sumWords++;
+            }
+            } catch (IOException ex) {
+            System.out.println("Invalid URL(MalURL)");
+        }
+        } catch (MalformedURLException ex) {
+            System.out.println("Invalid URL(IO Ex)");
+        }
+        return sumWords;
     }
-    
-    public static void main(String[] args)throws MalformedURLException  {
-        try { System.out.println("There are " + countWords(""
-                + "http://cs.armstrong.edu/liang/data/Lincoln.txt"
-                + "") + " words in Abraham Lincoln's Gettysburg address.");
-        }catch(IOException ex){
-           System.out.println("Invalid URL");
-       }catch(Exception ex){
-           System.out.println("Invalid URL");
-       }
+
+    public static void main(String[] args) throws MalformedURLException {
+        try {
+            System.out.println("There are " + countWords(""
+                    + "http://cs.armstrong.edu/liang/data/Lincoln.txt"
+                    + "") + " words in Abraham Lincoln's Gettysburg address.");
+        } catch (IOException ex) {
+            System.out.println("Invalid URL");
+        } catch (Exception ex) {
+            System.out.println("Invalid URL");
+        }
     }
 }
